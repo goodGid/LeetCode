d2088d22357b2d639a00032994887efa99a5e89e
Kiyong Shin <hello.goodgid@gmail.com>
2021-09-11 21:29:41 +0900

    Time: 1 ms (37.07%), Space: 48 MB (13.68%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.java
+++ merge-two-binary-trees/merge-two-binary-trees.java
@@ -13,17 +13,17 @@
  *     }
  * }
  */
-class Solution {    
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null) {
-            return t2;
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null){
+            return root2;
+        } else if (root2 == null) {
+            return root1;
         }
-        if (t2 == null) {
-            return t1;
-        }
-        t1.val += t2.val;
-        t1.left = mergeTrees(t1.left, t2.left);
-        t1.right = mergeTrees(t1.right, t2.right);
-        return t1;
+        
+        TreeNode node = new TreeNode(root1.val + root2.val);
+        node.left = mergeTrees(root1.left, root2.left);
+        node.right = mergeTrees(root1.right, root2.right);
+        return node;
     }
 }
