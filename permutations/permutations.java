3e697cbfd033a610ed844c6b3f25d81a4d75d3e7
Kiyong Shin <hello.goodgid@gmail.com>
2021-09-21 10:45:58 +0900

    Time: 3 ms (29.95%), Space: 41.6 MB (16.96%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,34 +1,33 @@
-     class Solution {
-
-        private int[] visit;
-
-        public List<List<Integer>> permute(int[] nums) {
-            visit = new int[nums.length];
-
-            List<List<Integer>> ans = new ArrayList<>();
-            List<Integer> list = new ArrayList<>();
-            solve(ans, nums, list);
-            return ans;
+class Solution {
+ 
+    int[] visit;
+    
+    public List<List<Integer>> permute(int[] nums) {
+        visit = new int[nums.length];
+        
+        List<List<Integer>> ans = new ArrayList<>();
+            
+        go(nums, new ArrayList<>(), ans);
+        return ans;
+    }
+    
+    // ac = Answer Candidate 
+    private void go(int[] nums, List<Integer> ac, List<List<Integer>> ans) {
+        if (ac.size() == nums.length) {
+            ans.add(new ArrayList(ac));            
+            return;
         }
-
-        private void solve(List<List<Integer>> ans, int[] nums, List<Integer> list) {
-            if (list.size() == nums.length) {
-                ans.add(new ArrayList<>(list));
-                return;
+        
+        for (int i=0; i<nums.length; i++) {
+            if (visit[i] == 1) {
+                continue;
             }
-
-            for (int i = 0; i < nums.length; i++) {
-                if (visit[i] == 1) {
-                    continue;
-                }
-
-                list.add(nums[i]);
-                visit[i] = 1;
-                solve(ans, nums, list);
-                list.remove(list.size() - 1);
-                visit[i] = 0;
-
-            }
-
+            
+            visit[i] = 1;
+            ac.add(nums[i]);
+            go(nums,ac,ans);
+            ac.remove(ac.size()-1);
+            visit[i] = 0;
         }
     }
+}
