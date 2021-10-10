3e87c643f25eba3d1de37dfebcd943a0b45af394
Kiyong Shin <hello.goodgid@gmail.com>
2021-10-10 20:04:29 +0900

    Time: 31 ms (84.19%), Space: 49.5 MB (53.31%) - LeetHub

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -1,16 +1,34 @@
 class Solution {
-
-    public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> st = new Stack<>();
-        int[] ans = new int[temperatures.length];
-
-        for (int i = 0; i < temperatures.length; i++) {
-            while (!st.isEmpty() && temperatures[st.peek()] < temperatures[i]) {
-                int idx = st.pop();
-                ans[idx] = i - idx;
+    public int[] dailyTemperatures(int[] t) {
+        int[] ans = new int[t.length];
+        Stack<Pair> s = new Stack<>();
+        
+        for (int i=0; i<t.length; i++) {
+            
+            while (!s.empty()) {
+                Pair topNode = s.peek();
+                
+                if (t[i] <= topNode.val) {
+                    break;
+                }
+                
+                if (t[i] > topNode.val) {
+                    s.pop();
+                    ans[topNode.idx] = i - topNode.idx;
+                }
             }
-            st.push(i);
+            s.push(new Pair(i, t[i]));
         }
         return ans;
+    }
+    
+    public class Pair {
+        int idx;
+        int val;
+        
+        public Pair(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
     }
 }
