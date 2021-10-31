87d5aaae6c8ca9b15f818fae09b76bed2373dbfb
Kiyong Shin <hello.goodgid@gmail.com>
2021-10-31 10:58:18 +0900

    Time: 19 ms (31.89%), Space: 47 MB (42.86%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,46 +1,23 @@
 class Solution {
+
     public int[] topKFrequent(int[] nums, int k) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.computeIfPresent(nums[i], (key, value) -> {
-                    return value + 1;
-                });
-            } else {
-                map.put(nums[i], 1);
-            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        List<Node> nodeList = new LinkedList<>();
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            nodeList.add(new Node(key, value));
-        }
-
-        List<Node> sortedNodeList = nodeList.stream()
-                                            .sorted((a, b) -> b.value - a.value)
-                                            .limit(k)
-                                            .collect(Collectors.toList());
-
+        List<Map.Entry<Integer, Integer>> collect = map.entrySet()
+                                                   .stream()
+                                                   .sorted((o1, o2) -> o2.getValue() - o1.getValue())
+                                                   .collect(Collectors.toList());
+        
         int[] ans = new int[k];
-        for (int i = 0; i < sortedNodeList.size(); i++) {
-            ans[i] = sortedNodeList.get(i).key;
+        for (int i = 0; i < k; i++) {
+            ans[i] = collect.get(i).getKey();
         }
+
         return ans;
-    }
-
-    public class Node {
-        int key;
-        int value;
-
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-
     }
 }
