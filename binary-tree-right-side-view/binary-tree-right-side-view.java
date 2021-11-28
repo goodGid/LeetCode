5bcf278a5d7dcc76e0eb4f6f4389e2190df8bebb
Kiyong Shin <hello.goodgid@gmail.com>
2021-11-28 12:18:51 +0900

    Time: 1 ms (78.58%), Space: 39.6 MB (8.63%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.java
+++ binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,34 +15,36 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) {
+        if ( root == null ) {
             return new ArrayList<>();
         }
         
-        Queue<TreeNode> q = new LinkedList<>();
         List<Integer> ans = new ArrayList<>();
+        List<List<Integer>> treeLevelGroup = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
         
         q.add(root);
         
         while (!q.isEmpty()) {
             int size = q.size();
             
+            List<Integer> list = new ArrayList<>();
+            
             for (int i=0; i<size; i++) {
                 TreeNode node = q.poll();
-                
-                if (i == size -1) {
-                    ans.add(node.val);
-                }
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                if (node.right != null) {
-                    q.add(node.right);
-                }
+                list.add(node.val);               
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
             }
+            treeLevelGroup.add(list);
         }
         
+        int depth = treeLevelGroup.size();
+        for (int i=0; i<depth; i++) {
+            int size = treeLevelGroup.get(i).size();
+            ans.add(treeLevelGroup.get(i).get(size-1));
+        }
+    
         return ans;
-        
     }
 }
