aaaa73d389ecd03476e04d2437471eae7677e41b
Kiyong Shin <hello.goodgid@gmail.com>
2021-12-04 14:23:57 +0900

    Time: 2 ms (84.44%), Space: 42.1 MB (41.22%) - LeetHub

--- minimum-path-sum/minimum-path-sum.java
+++ minimum-path-sum/minimum-path-sum.java
@@ -1,24 +1,26 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+
+        int r = grid.length;
+        int c = grid[0].length;
+        int[][] dp = new int[r][c];
         
-        int[][] dp = new int[n+1][m+1];
+        dp[0][0] = grid[0][0];
         
-        for (int i=0; i<m-1; i++) {
-            dp[n][i] = 987654321;
+        for (int i=1; i<r; i++) {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
         }
         
-        for (int i=0; i<n-1; i++) {
-            dp[i][m] = 987654321;
+        for (int i=1; i<c; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i-1];
         }
         
-        
-        for (int i=n-1; i>=0; i--) {
-            for (int j=m-1; j>=0; j--) {
-                dp[i][j] = Math.min(dp[i][j+1], dp[i+1][j]) + grid[i][j];
+        for (int i=1; i<r; i++) {
+            for (int j=1; j<c; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
-        }        
-        return dp[0][0];
+        }
+
+        return dp[r-1][c-1];
     }
 }
