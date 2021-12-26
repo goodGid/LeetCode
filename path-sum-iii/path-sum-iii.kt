b0417ba045d881d392ba75b950ba00df1cf938ce
Kiyong Shin <hello.goodgid@gmail.com>
2021-12-26 21:59:10 +0900

    Time: 208 ms (61.29%), Space: 38.8 MB (51.61%) - LeetHub

---
+++ path-sum-iii/path-sum-iii.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun pathSum(root: TreeNode?, targetSum: Int): Int {
+        return initiate(root, targetSum)
+    }
+
+    private fun initiate(root: TreeNode?, targetSum: Int): Int {
+        if (root == null) return 0
+        return fromNodePathSum(root, targetSum) + initiate(root.left, targetSum) + initiate(root.right, targetSum)
+    }
+
+    private fun fromNodePathSum(node: TreeNode?, targetSum: Int): Int {
+        if (node == null) return 0
+        val me = if (node.`val` == targetSum) 1 else 0
+        val left = fromNodePathSum(node.left, targetSum - node.`val`)
+        val right = fromNodePathSum(node.right, targetSum - node.`val`)
+        return me + left + right
+    }
+}
