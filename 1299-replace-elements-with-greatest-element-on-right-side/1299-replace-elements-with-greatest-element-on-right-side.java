e03b175e6d8c915634126f8efa90d5f553493a7e
Kiyong Shin <hello.goodgid@gmail.com>
2022-03-06 09:35:16 +0900

    Time: 1 ms (99.82%), Space: 54.9 MB (7.43%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,45 +1,12 @@
-class Solution {
-    public int[] replaceElements(int[] arr) {
-        if (arr.length == 1) {
-            return new int[]{-1};
-        }
-        int[] ans = new int[arr.length];
 
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-
-        for (int i = 1; i < arr.length; i++) {
-            queue.add(new Node(i, arr[i]));
-        }
-
-        ans[0] = queue.peek().value;
-        ans[arr.length-1] = -1;
-        int ansIdx = 1;
-
-        while (!queue.isEmpty()) {
-            Node node = queue.peek();
-            if (ansIdx >= node.index) {
-                queue.poll();
-                continue;
+    class Solution {
+        public int[] replaceElements(int[] arr) {
+            int max = -1;
+            for (int i = arr.length - 1; i >= 0; i--) {
+                int temp = arr[i];
+                arr[i] = max;
+                max = Math.max(max, temp);
             }
-            ans[ansIdx] = queue.peek().value;
-            ansIdx++;
-        }
-
-        return ans;
-    }
-
-    public class Node implements Comparable<Node> {
-        private int index;
-        private int value;
-
-        public Node(int index, int value) {
-            this.index = index;
-            this.value = value;
-        }
-
-        @Override
-        public int compareTo(Node newNode) {
-            return newNode.value - value;
+            return arr;
         }
     }
-}
