46b5a4ea65e1336c6bc4d20d440c2f4587cdc5c0
Kiyong Shin <hello.goodgid@gmail.com>
2022-03-27 15:23:24 +0900

    Time: 4 ms (99.31%), Space: 54.3 MB (91.19%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,45 +1,19 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        char ZERO = '0';
-        char ONE = '1';
-
-        int ans = 0;
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int maxSize = Math.min(n, m);
-        int[][][] dp = new int[n][m][maxSize + 1];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == ZERO) {
-                    dp[i][j][1] = ZERO;
-                } else {
-                    ans = Math.max(ans, 1);
-                    dp[i][j][1] = ONE;
+        int rows = matrix.length;
+        int cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int res = 0;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    res = Math.max(res, dp[i][j]);
                 }
             }
         }
-
-        for (int s = 2; s <= maxSize; s++) {
-            for (int i = s - 1; i < n; i++) {
-                for (int j = s - 1; j < m; j++) {
-
-                    if (matrix[i][j] == ZERO) {
-                        dp[i][j][s] = ZERO;
-                        continue;
-                    }
-
-                    if (dp[i - 1][j][s - 1] == ONE
-                        && dp[i][j - 1][s - 1] == ONE
-                        && dp[i - 1][j - 1][s - 1] == ONE) {
-                        ans = Math.max(ans, s * s);
-                        dp[i][j][s] = ONE;
-                    } else {
-                        dp[i][j][s] = ZERO;
-                    }
-                }
-            }
-        }
-        return ans;
+        
+        return res * res;
     }
 }
