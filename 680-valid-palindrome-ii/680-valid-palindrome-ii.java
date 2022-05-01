3c1f0b6265663b64e4b98d6a6b10a99fe14f5f2f
Kiyong Shin <hello.goodgid@gmail.com>
2022-05-02 00:42:04 +0900

    Time: 6 ms (91.03%), Space: 42.6 MB (94.81%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,14 +1,14 @@
-class Solution {    
+class Solution {
     public boolean validPalindrome(String s) {
-        return check(s, 0, s.length()-1, false);
+        return check(s, 0, s.length() - 1, false);
     }
-    
+
     private boolean check(String s, int l, int r, boolean removed) {
         if (l > r) {
             return true;
         }
-        
-        while (l<=r) {
+
+        while (l <= r) {
             if (s.charAt(l) == s.charAt(r)) {
                 l++;
                 r--;
@@ -19,12 +19,6 @@ class Solution {
                 break;
             }
         }
-        
-        
-        return check(s, l+1, r, true) || check(s, l, r - 1, true);
-        
+        return check(s, l + 1, r, true) || check(s, l, r - 1, true);
     }
-    
-    
-    
 }
