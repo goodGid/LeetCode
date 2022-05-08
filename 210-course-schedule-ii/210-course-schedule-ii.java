25589933c9305ac6d1976b2a1f04c0290e02efb5
Kiyong Shin <hello.goodgid@gmail.com>
2022-05-08 21:53:48 +0900

    Time: 6 ms (77.05%), Space: 48.6 MB (63.72%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,46 +1,34 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjacencyList = new ArrayList<>();
+        int size = prerequisites.length;
+        Queue<Integer> q = new LinkedList<>();
+        int[] arr = new int[numCourses];
+        List<List<Integer>> list = new ArrayList<>();
 
         for (int i = 0; i < numCourses; i++) {
-            adjacencyList.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-
-        int[] inDegree = new int[numCourses];
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            int prev = prerequisites[i][1];
-            int next = prerequisites[i][0];
-            adjacencyList.get(prev).add(next);
-            inDegree[next]++;
+        for (int i = 0; i < size; i++) {
+            arr[prerequisites[i][0]]++;
+            list.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                queue.offer(i);
+            if (arr[i] == 0) {
+                q.add(i);
             }
         }
-
-        int idx = 0;
         int[] ans = new int[numCourses];
+        int idx = 0;
+        while (!q.isEmpty()) {
+            int top = q.poll();
+            ans[idx++] = top;
 
-        while (!queue.isEmpty()) {
-            Integer top = queue.poll();
-            ans[idx] = top;
-            idx++;
-
-            for (int i : adjacencyList.get(top)) {
-                inDegree[i]--;
-
-                if (inDegree[i] == 0) {
-                    queue.offer(i);
+            for (int i : list.get(top)) {
+                arr[i]--;
+                if (arr[i] == 0) {
+                    q.add(i);
                 }
             }
         }
@@ -48,6 +36,7 @@ class Solution {
         if (idx != numCourses) {
             return new int[] {};
         }
+
         return ans;
     }
 }
