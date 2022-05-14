b7ce7320c372cb6d503ccc95346409917def9a55
Kiyong Shin <hello.goodgid@gmail.com>
2022-05-14 16:17:26 +0900

    Time: 3 ms (80.95%), Space: 49.9 MB (43.31%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,32 +1,55 @@
-// ref : https://bit.ly/3weywNF
+// ref : https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/1701107/Java-Binary-Search-Explained
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int index1 = 0;
-        int index2 = 0;
-        int med1 = 0;
-        int med2 = 0;
-        for (int i = 0; i <= (nums1.length + nums2.length) / 2; i++) {
-            med1 = med2;
-            if (index1 == nums1.length) {
-                med2 = nums2[index2];
-                index2++;
-            } else if (index2 == nums2.length) {
-                med2 = nums1[index1];
-                index1++;
-            } else if (nums1[index1] < nums2[index2]) {
-                med2 = nums1[index1];
-                index1++;
+        if (nums1 == null && nums2 == null) {
+            return 0;
+        }
+
+        if (nums1 == null) {
+            int n = nums2.length;
+            return nums2[(n - 1) / 2] * 0.5 + nums2[n / 2] * 0.5;
+        }
+
+        if (nums2 == null) {
+            int n = nums1.length;
+            return nums1[(n - 1) / 2] * 0.5 + nums1[n / 2] * 0.5;
+        }
+
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        // choose shorter to binary search
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = 0;
+        int right = n;
+
+        while (left < right) {
+            int i = (left + right) / 2;
+            int j = (n + m) / 2 - i;
+
+            if (nums1[i] < nums2[j - 1]) {
+                left = i + 1;
             } else {
-                med2 = nums2[index2];
-                index2++;
+                right = i;
             }
         }
 
-        // the median is the average of two numbers
-        if ((nums1.length + nums2.length) % 2 == 0) {
-            return (float) (med1 + med2) / 2;
+        int first = left;
+        int second = (n + m) / 2 - left;
+
+        int shorterLeft = first == 0 ? Integer.MIN_VALUE : nums1[first - 1];
+        int shorterRight = first == n ? Integer.MAX_VALUE : nums1[first];
+
+        int longerLeft = second == 0 ? Integer.MIN_VALUE : nums2[second - 1];
+        int longerRight = second == m ? Integer.MAX_VALUE : nums2[second];
+
+        if ((n + m) % 2 == 1) {
+            return Math.min(shorterRight, longerRight);
+        } else {
+            return Math.max(shorterLeft, longerLeft) * 0.5 + Math.min(shorterRight, longerRight) * 0.5;
         }
 
-        return med2;
     }
 }
