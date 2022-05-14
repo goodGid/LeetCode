a8020ed83d70599ba2e445378a4402cbffb0490c
Kiyong Shin <hello.goodgid@gmail.com>
2022-05-14 15:26:37 +0900

    Time: 3 ms (80.95%), Space: 49.8 MB (43.31%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,76 +1,31 @@
 class Solution {
-    public double findMedianSortedArrays(int[] n1, int[] n2) {
-
-        int size = n1.length + n2.length;
-        boolean isEven = size % 2 == 0;
-        int mid = size / 2;
-
-        int idx1 = 0;
-        int idx2 = 0;
-        int[] ansArr = new int[mid + 1];
-        int ansArrIdx = 0;
-
-        if (n1.length == 0) {
-            if (isEven) {
-                return (n2[mid] + n2[mid - 1]) / 2.0;
-            } else {
-                return n2[mid] / 1.0;
-            }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    int index1 = 0;
+    int index2 = 0;
+    int med1 = 0;
+    int med2 = 0;
+    for (int i=0; i<=(nums1.length+nums2.length)/2; i++) {
+        med1 = med2;
+        if (index1 == nums1.length) {
+            med2 = nums2[index2];
+            index2++;
+        } else if (index2 == nums2.length) {
+            med2 = nums1[index1];
+            index1++;
+        } else if (nums1[index1] < nums2[index2] ) {
+            med2 = nums1[index1];
+            index1++;
+        }  else {
+            med2 = nums2[index2];
+            index2++;
         }
-
-        if (n2.length == 0) {
-            if (isEven) {
-                return (n1[mid] + n1[mid - 1]) / 2.0;
-            } else {
-                return n1[mid] / 1.0;
-            }
-        }
-
-        while (isRange(idx1, n1) && isRange(idx2, n2)) {
-            if (idx1 + idx2 == mid + 1) {
-                if (isEven) {
-                    return (ansArr[ansArrIdx - 1] + ansArr[ansArrIdx - 2]) / 2.0;
-                } else {
-                    return ansArr[ansArrIdx - 1] / 1.0;
-                }
-            }
-            if (n1[idx1] > n2[idx2]) {
-                ansArr[ansArrIdx++] = n2[idx2];
-                idx2++;
-            } else {
-                ansArr[ansArrIdx++] = n1[idx1];
-                idx1++;
-            }
-        }
-
-        while (isRange(idx1, n1)) {
-            if (idx1 + idx2 == mid + 1) {
-                if (isEven) {
-                    return (ansArr[ansArrIdx - 1] + ansArr[ansArrIdx - 2]) / 2.0;
-                } else {
-                    return ansArr[ansArrIdx - 1] / 1.0;
-                }
-            }
-            ansArr[ansArrIdx++] = n1[idx1];
-            idx1++;
-        }
-
-        while (isRange(idx2, n2)) {
-            if (idx1 + idx2 == mid + 1) {
-                if (isEven) {
-                    return (ansArr[ansArrIdx - 1] + ansArr[ansArrIdx - 2]) / 2.0;
-                } else {
-                    return ansArr[ansArrIdx - 1] / 1.0;
-                }
-            }
-            ansArr[ansArrIdx++] = n2[idx2];
-            idx2++;
-        }
-
-        return (ansArr[0] + ansArr[1]) / 2.0;
     }
 
-    private boolean isRange(int idx, int[] arr) {
-        return idx < arr.length;
+    // the median is the average of two numbers
+    if ((nums1.length+nums2.length)%2 == 0) {
+        return (float)(med1+med2)/2;
     }
+
+    return med2;
+}
 }
