340fe5356d6dc8a7de5b1c0e70cbb688ab7424b1
Kiyong Shin <hello.goodgid@gmail.com>
2022-05-23 00:00:20 +0900

    Time: 16 ms (55.83%), Space: 192.4 MB (47.70%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -10,7 +10,7 @@ class Solution {
     void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
         if (start >= s.length()) {result.add(new ArrayList<>(currentList));}
         for (int end = start; end < s.length(); end++) {
-            if (s.charAt(start) == s.charAt(end) && (end - start <= 1 || dp[start + 1][end - 1])) {
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
                 dp[start][end] = true;
                 currentList.add(s.substring(start, end + 1));
                 dfs(result, s, end + 1, currentList, dp);
