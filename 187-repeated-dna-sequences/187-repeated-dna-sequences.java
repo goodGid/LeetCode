28011af6e535b86cb2f0e09561ae42434fd11bbb
Kiyong Shin <hello.goodgid@gmail.com>
2022-06-18 22:59:13 +0900

    Time: 35 ms (56.96%), Space: 64.6 MB (16.51%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -1,51 +1,13 @@
-// ref : https://leetcode.com/problems/repeated-dna-sequences/discuss/53980/Accepted-Java-easy-to-understand-solution
+// ref : https://leetcode.com/submissions/detail/725160469
 
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> list = new ArrayList<String>();
-        if (s == null || s.length() < 10) {return list;}
-        HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
-
-        for (int i = 0; i + 10 <= s.length(); i++) {
-            int hash = stringToHash(s.substring(i, i + 10));
-            // int hash = stringToHash2(s.substring(i, i + 10));
-            if (map.containsKey(hash)) {
-                if (!map.get(hash)) {
-                    list.add(s.substring(i, i + 10));
-                    map.put(hash, true);
-                }
-            } else {
-                map.put(hash, false);
-            }
+        Set<String> seen = new HashSet<>(), ans = new HashSet<>();
+        String sub = null;
+        for (int i = 0; i <= s.length() - 10; i++) {
+            sub = s.substring(i, i + 10);
+            if (!seen.add(sub)) {ans.add(sub);}
         }
-        return list;
-    }
-
-    private int stringToHash(String s) {
-        String numberBuilder = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'A') {numberBuilder += "0";}
-            if (s.charAt(i) == 'C') {numberBuilder += "1";}
-            if (s.charAt(i) == 'G') {numberBuilder += "2";}
-            if (s.charAt(i) == 'T') {numberBuilder += "3";}
-        }
-        return Integer.parseInt(numberBuilder, 4);
-    }
-
-    private int stringToHash2(String s) {
-        int numberBuilder = 0;
-        for (int i = 0; i < s.length(); i++) {
-            numberBuilder *= 4;
-            if (s.charAt(i) == 'A') {
-                numberBuilder += 0;
-            } else if (s.charAt(i) == 'C') {
-                numberBuilder += 1;
-            } else if (s.charAt(i) == 'G') {
-                numberBuilder += 2;
-            } else if (s.charAt(i) == 'T') {
-                numberBuilder += 3;
-            }
-        }
-        return numberBuilder;
+        return new ArrayList<>(ans);
     }
 }
