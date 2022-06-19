f316194882bddfd1f1f87410bba74b2984163aa6
Kiyong Shin <hello.goodgid@gmail.com>
2022-06-20 00:33:59 +0900

    Time: 51 ms (15.12%), Space: 43.1 MB (64.40%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -1,56 +1,26 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
     public Node cloneGraph(Node node) {
-        if (node == null) {
-            return null;
+        if (node == null) {return node;}
+
+        Map<Node, Node> map = new HashMap<>();
+
+        return cloneGraph(node, map);
+    }
+
+    private Node cloneGraph(Node node, Map<Node, Node> map) {
+
+        if (map.containsKey(node)) {
+            return map.get(node);
         }
 
-        HashMap<Integer, Node> map = new HashMap<>();
-        int[] visit = new int[101];
-        Queue<Node> q = new LinkedList<>();
+        Node cloneNode = new Node(node.val);
+        map.put(node, cloneNode);
 
-        Node root = new Node(node.val);
-        map.put(node.val, root);
-        q.add(node);
-
-        while (!q.isEmpty()) {
-            Node temp = q.poll();
-            visit[temp.val] = 1;
-            Node clonedNode = map.get(temp.val);
-
-            List<Node> neighbors = temp.neighbors;
-            for (Node neighbor : neighbors) {
-                if (visit[neighbor.val] == 0) {
-                    Node newNode = new Node(neighbor.val);
-                    map.put(newNode.val, newNode);
-                    visit[newNode.val] = 1;
-                    clonedNode.neighbors.add(newNode);
-                    q.add(neighbor);
-                } else {
-                    Node newNode = map.get(neighbor.val);
-                    clonedNode.neighbors.add(newNode);
-                }
-            }
+        for (Node neigh : node.neighbors) {
+            Node cloneNeigh = cloneGraph(neigh, map);
+            cloneNode.neighbors.add(cloneNeigh);
         }
-        return root;
+
+        return cloneNode;
     }
 }
