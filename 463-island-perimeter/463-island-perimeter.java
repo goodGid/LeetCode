21753098f902f50c6b9c8d18325d8140ec8ad436
Kiyong Shin <hello.goodgid@gmail.com>
2022-06-20 15:21:00 +0900

    Time: 10 ms (63.09%), Space: 62.3 MB (51.76%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -1,62 +1,20 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        Queue<Node> q = new LinkedList<>();
-        int ans = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-        int[][] visit = new int[row][col];
-        
-        int[] dx = new int[] {1,-1,0,0};
-        int[] dy = new int[] {0,0,1,-1};
-        
-        for (int i=0; i<row; i++) {
-            for (int j=0; j<col; j++) {
+        if (grid.length == 0 || grid == null) {return 0;}
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    q.add(new Node(i,j));
-                    visit[i][j] = 1;
-                    break;
+                    result += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        result -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        result -= 2;
+                    }
                 }
             }
         }
-            
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            for (int i=0; i<4; i++) {
-                int nx = dx[i] + node.x;
-                int ny = dy[i] + node.y;
-                
-                if (!isRange(grid, nx,ny)) {
-                    ans++;
-                } else if (grid[nx][ny] == 0) {
-                    ans++;
-                } else if (grid[nx][ny] == 1 && visit[nx][ny] == 0) {
-                    visit[nx][ny] = 1;
-                    q.add(new Node(nx,ny));
-                }
-            }   
-        }
-        return ans;
-    }
-    
-    private boolean isRange(int[][] grid, int x, int y) {
-        int row = grid.length;
-        int col = grid[0].length;
-        
-        if (x < 0 || x >= row) {
-            return false;
-        }
-        if (y < 0 || y >= col) {
-            return false;
-        }
-        return true;
-    }
-    
-    public class Node {
-        int x;
-        int y;
-        public Node(int _x, int _y) {
-            x = _x;
-            y = _y;
-        }
+        return result;
     }
 }
