f51a867f76da117d57b388e479df7f93ff77421d
Kiyong Shin <hello.goodgid@gmail.com>
2023-04-30 01:22:18 +0900

    Time: 6 ms (74.59%), Space: 43.2 MB (13.51%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,15 @@
+// ref : https://leetcode.com/problems/remove-covered-intervals/discuss/1784874/Beginner-friendly-JavaJavaScriptPython-Solution
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> (a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]));
+        int count = 0, cur = 0;
+        for(int interval[] : intervals){
+            if(cur < interval[1]){
+                cur = interval[1];
+                count++;
+            }
+        }
+        return count;
+    }
+}
