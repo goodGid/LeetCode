2b977f710a0aec6a28a1c4259970b84a9cb4be85
Kiyong Shin <hello.goodgid@gmail.com>
2023-07-26 07:07:35 +0900

    Time: 0 ms (100.00%), Space: 43.4 MB (54.83%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,54 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) {
-            return false;
-        }
-                
-        boolean hasChild = false;
-        if (root.left != null || root.right != null) {
-            hasChild = true;
-        }
-        
-        return dfs(root, targetSum, 0, hasChild);
+        if (root == null) return false;
+
+        return dfs(root, targetSum);
     }
-    
-    private boolean dfs(TreeNode node, int t, int sum, boolean hasChild) {
-        if (node == null) {
-            if (hasChild == false && t == sum) {
-                return true;
-            }
+
+    private boolean dfs(TreeNode node, int targetSum) {
+        if (node == null) { // base case
             return false;
         }
-        
-        hasChild = false;
-        if (node.left != null || node.right != null) {
-            hasChild = true;
+
+        targetSum -= node.val;
+
+        // check whether it's a leaf node
+        if (node.left == null && node.right == null) {
+            return targetSum == 0;
         }
-        
-        boolean isSame = false;
-        isSame = dfs(node.left, t, sum + node.val, hasChild);
-        if (isSame) {
-            return true;
-        }
-        isSame = dfs(node.right, t, sum + node.val, hasChild);
-        if (isSame) {
-            return true;
-        }
-        return false;
+
+        return dfs(node.left, targetSum) || dfs(node.right, targetSum);
     }
 }
+
