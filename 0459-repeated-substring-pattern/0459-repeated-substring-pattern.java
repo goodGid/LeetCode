e0e2247d590c9207b6afb8d1e2d68b40785f4f91
Kiyong Shin <hello.goodgid@gmail.com>
2023-08-22 00:03:02 +0900

    Time: 7 ms (95.07%), Space: 44 MB (83.73%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,9 +1,16 @@
+/*
+ref 
+- https://leetcode.com/submissions/detail/1027601276
+- https://leetcode.com/submissions/detail/1027193919
+
+- [1] : size를 i로 나눴는데 0으로 안나눠지면 어차피 조건 충족 X
+*/
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int size = s.length();
 
         for (int i = 1; i <= size / 2; i++) {
-            if (size % i != 0) {continue;}
+            if (size % i != 0) {continue;} // [1]
             if (isCheck(s, i)) {
                 return true;
             }
