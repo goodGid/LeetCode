452c4fa4f032c20ab4583b4684d3d599745f082a
Kiyong Shin <hello.goodgid@gmail.com>
2023-08-27 16:00:49 +0900

    Time: 4 ms (99.94%), Space: 60 MB (72.29%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        long sum = 0;
+        int size = nums.length;
+        
+        int[] ans = new int[size];
+        for (int i=0; i<size; i++) {
+            ans[i] = -1;
+        }
+        
+        for (int i=0; i<k*2 && i<size; i++) {
+            sum += nums[i];
+        }
+        
+        for (int i=k; i<size-k && i<size; i++) {
+            sum += nums[i+k];
+            // System.out.println(sum);
+            ans[i] = (int) (sum / (k*2+1));
+            sum -= nums[i-k];
+        }
+        
+        return ans;
+    }
+}
