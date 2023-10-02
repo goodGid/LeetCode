c036d1c2a7792a08c2af0548a3c4806315702108
Kiyong Shin <hello.goodgid@gmail.com>
2023-10-03 02:13:37 +0900

    Time: 0 ms (100.00%), Space: 40.4 MB (75.42%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,69 +1,44 @@
+// ref : 정답 코드 중 Runtime 효율 1위 블록 참고
 class Solution {
     public String reorganizeString(String s) {
+        int[] freq = new int[26]; //to Store Frequency of each alphabet
         char[] arr = s.toCharArray();
-        int size = s.length();
-        Map<Character, Integer> map = new HashMap<>();
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue());
 
-        if (size == 3) {
-            if (arr[0] == arr[1] && arr[1] == arr[2]) {
-                return "";
-            }
-            for (int i = 0; i < 3; i++) {
-                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            }
-            for (Character key : map.keySet()) {
-                Integer i = map.get(key);
-                pq.add(new Node(key, i));
-            }
-        } else {
-            int maxCnt = 0;
-            for (char c : arr) {
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            }
-            for (Character key : map.keySet()) {
-                Integer i = map.get(key);
-                pq.add(new Node(key, i));
-            }
+        for (int i = 0; i < arr.length; i++) {  //store the frequency
+            freq[arr[i] - 'a']++;
+        }
 
-            if (size % 2 == 1 && pq.peek().cnt > size / 2 + 1) {
-                return "";
-            }
-
-            if (size % 2 == 0 && pq.peek().cnt > size / 2) {
-                return "";
+        int max = 0, letter = 0;
+        for (int i = 0; i < 26; i++) {  //find the max frequency
+            if (freq[i] > max) {
+                max = freq[i];
+                letter = i;
             }
         }
 
-        char[] ans = new char[size];
+        if (max > (s.length() + 1) / 2) {
+            return ""; //if max is more than half then not possible
+        }
+
         int idx = 0;
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            for (int i = 0; i < node.cnt; i++) {
-                ans[idx] = node.c;
-                idx = idx + 2;
-                if (idx >= size) {
-                    idx %= size;
-                    if (idx % 2 == 0) {
-                        idx++;
-                    }
+        char[] res = new char[s.length()];
+
+        while (freq[letter] > 0) {   //distribute the max freq char into even indices
+            res[idx] = (char) (letter + 'a');
+            idx += 2;
+            freq[letter]--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] > 0) {
+                if (idx >= s.length()) {
+                    idx = 1; //all even indices filled, so switch to odd indices
                 }
+                res[idx] = (char) (i + 'a');
+                idx += 2;
+                freq[i]--;
             }
         }
-        return String.valueOf(ans);
-    }
-
-    class Node {
-        char c;
-        int cnt;
-
-        public Node(char _c, int _cnt) {
-            c = _c;
-            cnt = _cnt;
-        }
-
-        public int getValue() {
-            return this.cnt;
-        }
+        return String.valueOf(res);
     }
 }
