bd163bd3ce327159d8cb832b31251075ae9da81b
Kiyong Shin <hello.goodgid@gmail.com>
2023-11-26 11:31:27 +0900

    Time: 1 ms (99.87%), Space: 55.6 MB (53.88%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,30 +1,23 @@
+// from Accepted Solutions Runtime Distribution
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int n = nums.length;
-        
-        if (n < 2) {
-            return true;
-        }
-        
-        int st = 1;
-        
-        boolean preDir = true;
-        for (int i = st; i<n; i++) {
-            if (nums[i] == nums[i-1]) {
-                continue;
+
+        int lowValue = nums[0];
+        int highValue = nums[nums.length - 1];
+        boolean increase = true;
+
+        if (lowValue <= highValue) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i+1]) {
+                    return false;
+                }
             }
-            preDir = nums[i] - nums[i-1] >= 0? true: false;
-            st = i;
-            break;
         }
-               
-        for (int i=st; i<n; i++) {
-            if (nums[i] == nums[i-1]) {
-                continue;
-            }
-            boolean curDir = nums[i] - nums[i-1] >= 0? true: false;
-            if (preDir != curDir) {
-                return false;
+        else {
+           for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] < nums[i+1]) {
+                    return false;
+                }
             }
         }
         return true;
