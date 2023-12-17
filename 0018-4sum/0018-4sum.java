2f03d1579f6e2804a21e2721630dbfffdcc71305
Kiyong Shin <hello.goodgid@gmail.com>
2023-12-17 15:19:10 +0900

    Time: 14 ms (82.88%), Space: 43.7 MB (85.48%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,46 +1,32 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] num, int target) {
-        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
-        int n = num.length;
-        if (n == 0 || n < 3) {
-            return res;
-        }
-        if (target == -294967296 || target == 294967296) {
-            return res;
-        }
-        if (num == null || num.length == 0) {return res;}
-
-        Arrays.sort(num);
-
-        for (int i = 0; i < n; i++) {
-            int target_3 = target - num[i];
-            for (int j = i + 1; j < n; j++) {
-                int target_2 = target_3 - num[j];
-
-                int front = j + 1;
-                int back = n - 1;
-                while (front < back) {
-                    int two_sum = num[front] + num[back];
-                    if (two_sum < target_2) {front++;} else if (two_sum > target_2) {back--;} else {
-                        List<Integer> quad = new ArrayList<>();
-                        quad.add(num[i]);
-                        quad.add(num[j]);
-                        quad.add(num[front]);
-                        quad.add(num[back]);
-                        res.add(quad);
-                        // Processing the duplicates of number 3
-                        while (front < back && num[front] == quad.get(2)) {++front;}
-
-                        // Processing the duplicates of number 4
-                        while (front < back && num[back] == quad.get(3)) {--back;}
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums); // Array Sorting to bypass duplicate quadruplet. 
+        List<List<Integer>> output = new ArrayList<>();
+        for(int i=0;i<nums.length-3;i++){  // To get first element of quadruplet.
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            for(int j=i+1;j<nums.length-2;j++){ // To get second element of quadruplet.
+                if(j>i+1 && nums[j]==nums[j-1])
+                    continue;
+                long t = (long) target - nums[i] - nums[j]; // target is type casted to long to pass edge cases.
+                int k=j+1,l=nums.length-1;  // Used 2 pointer approach to find 3rd & 4th element of quadruplet.
+                while(k<l){
+                    if(nums[k]+nums[l] == t){
+                        output.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        while(k<l && nums[k]==nums[k+1])
+                            k++;
+                        while(k<l && nums[l]==nums[l-1])
+                            l--;
+                        k++;
+                        l--;
                     }
+                    else if(nums[k]+nums[l] > t)
+                        l--;
+                    else
+                        k++;
                 }
-                // Processing the duplicates of number 2
-                while (j + 1 < n && num[j + 1] == num[j]) {++j;}
             }
-            // Processing the duplicates of number 1
-            while (i + 1 < n && num[i + 1] == num[i]) {++i;}
         }
-        return res;
+        return output;
     }
 }
