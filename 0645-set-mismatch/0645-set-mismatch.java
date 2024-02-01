ab0b366d15578052a7ffe2d15b2c8640ac93ab69
Kiyong Shin <hello.goodgid@gmail.com>
2024-02-01 22:22:02 +0900

    Time: 2 ms (95.27%), Space: 45.7 MB (33.48%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,24 +1,17 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int size = nums.length;
-        
-        Set<Integer> set = new HashSet<>();
-        
-        for (int i=1; i<=size; i++) {
-            set.add(i);
-        }
-        
-        int[] ans = new int[2];
+        int[] map = new int[nums.length+1];
         for (int i : nums) {
-            if (set.contains(i)) {
-                set.remove(i);
-            } else {
+            map[i]++;
+        }
+        int[] ans = new int[2];
+        for (int i=1; i<nums.length+1; i++) {
+            if (map[i] == 0) {
+                ans[1] = i;
+            }
+            if (map[i] == 2) {
                 ans[0] = i;
             }
-        }
-        
-        for (int i : set) {
-            ans[1] = i;
         }
         
         return ans;
