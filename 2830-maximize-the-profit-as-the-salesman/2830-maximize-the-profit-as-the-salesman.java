363141a220daa4b2e4570342ce1b31b88d783245
Kiyong Shin <hello.goodgid@gmail.com>
2024-02-04 14:45:25 +0900

    Time: 148 ms (29.25%), Space: 104.4 MB (16.98%) - LeetHub

--- 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.java
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.java
@@ -1,54 +1,57 @@
-// 240131
-// 내가 풀었던 코드 : https://leetcode.com/submissions/detail/1162060200
-// 전체적인 로직은 동일하다.
-// 다만 현재 idx를 take 하였을 경우 다음에 접근 하는 idx를 찾는 로직에서
-// 나는 for문으로 찾았고 정답 코드는 BinarySearch로 찾았다.
-// from : https://leetcode.com/problems/maximize-the-profit-as-the-salesman/discuss/3935104/Clean-Code-with-Explanation
-
 class Solution {
-    int[] memo;
     int m;
+    int[] dp;
+
     public int maximizeTheProfit(int n, List<List<Integer>> offers) {
-        Collections.sort(offers, (a, b)-> a.get(0)-b.get(0));
-        this.m = offers.size();
-        memo = new int[m];
-        Arrays.fill(memo, -1);
-        return dfs(0, offers);
+        Collections.sort(offers, (a, b) -> a.get(0) - b.get(0));
+        Collections.sort(offers, Comparator.comparing(a -> a.get(0)));
+
+        m = offers.size();
+        dp = new int[m];
+        Arrays.fill(dp, -1);
+        dfs(0, offers);
+        return dp[0];
     }
 
-    int dfs(int i, List<List<Integer>> offers) {
-        if(i == m) {
+    private int dfs(int idx, List<List<Integer>> offers) {
+        if (idx == m) {
             return 0;
         }
-        if(memo[i] != -1) {
-            return memo[i];
+        if (dp[idx] != -1) {
+            return dp[idx];
         }
-        // not sell / don't take the offer
-        int notSellProfit = dfs(i+1, offers);
-        // sell / take the offer
-        int nextPossibleIdx = binSearch(i+1, offers.get(i).get(1), offers);
-        int sellProfit = offers.get(i).get(2);
-        if(nextPossibleIdx != -1) {
-            sellProfit += dfs(nextPossibleIdx, offers);
+
+        // Pick X
+        int sum1 = dfs(idx + 1, offers);
+
+        // Pick O
+        int nextIdx = findNextIdx(idx + 1, offers.get(idx).get(1), offers);
+        int sum2 = offers.get(idx).get(2);
+        if (nextIdx != -1) {
+            sum2 += dfs(nextIdx, offers);
         }
-        return memo[i] = Math.max(sellProfit, notSellProfit);
+
+        dp[idx] = Math.max(sum1, sum2);
+
+        return dp[idx];
     }
 
-    // Looks for next possible offer starting from left = idx+1 till right = m-1;
-    int binSearch(int left, int end, List<List<Integer>> offers) {
-        int right = m-1;
-        int i=-1;
-        while(left <= right) {
-            int mid = right - (right-left)/2;
-            int start = offers.get(mid).get(0);
-            if(start > end) {
-                i = mid;
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
+    private int findNextIdx(int left, int target, List<List<Integer>> offers) {
+        int l = left;
+        int r = m - 1;
+        int nextIdx = -1;
+        while (l <= r) {
+            // int mid = right - (right-left)/2;
+            // int mid = r - (r-l)/ 2;
+            int mid = l + (r - l) / 2;
+            int mv = offers.get(mid).get(0);
+            if (mv > target) {
+                nextIdx = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
-        return i;
+        return nextIdx;
     }
 }
