4c8c5de86f8bb8d5053af638d7fa5d6e46fb0b17
Kiyong Shin <hello.goodgid@gmail.com>
2024-02-10 00:19:21 +0900

    Time: 22 ms (89.45%), Space: 42.4 MB (86.03%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,13 +3,13 @@ class Solution {
         int[] dp = new int[n+1];
         
         for (int i=1; i<=n; i++) {
-            dp[i] = 10001;
+            dp[i] = i;
         }
-    
-        for (int i=1; i*i <= n; i++) {
+        
+        for (int i=1; i*i<=n; i++) {
             dp[i*i] = 1;
-            for(int j=i*i+1; j<=n; j++) {
-                dp[j] = Math.min(dp[j], dp[j-i*i] + 1);
+            for (int st=i*i; st<=n; st++) {
+                dp[st] = Math.min(dp[st], 1 + dp[st-(i*i)]);
             }
         }
         return dp[n];
