49eb30712dd6d0be87bbd5030078f7ad6250aa09
Kiyong Shin <hello.goodgid@gmail.com>
2024-02-26 22:26:22 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (95.56%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,61 +15,11 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-        Queue<TreeNode> q1 = new LinkedList<>();
-        Queue<TreeNode> q2 = new LinkedList<>();
-        
-        if (p != null) {
-            q1.add(p);
-        }
-        
-        if (q != null) {
-            q2.add(q);
-        }
-        
-        List<Integer> list = new ArrayList<>();
-        while (!q1.isEmpty()) {
-            TreeNode node = q1.poll();
-            
-            if (node == null) {
-                list.add(10005);
-                continue;
-            } else {
-                list.add(node.val);
-            }
-            
-            q1.add(node.left);
-            q1.add(node.right);
-        }
-        
-        List<Integer> list2 = new ArrayList<>();
-        while (!q2.isEmpty()) {
-            TreeNode node = q2.poll();
-            
-            if (node == null) {
-                list2.add(10005);
-                continue;
-            } else {
-                list2.add(node.val);
-            }
-            
-            q2.add(node.left);
-            q2.add(node.right);
-        }
-        
-        if (list.size() != list2.size()) {
+        if (p == null || q == null) {
+            return p == null && q == null;
+        } else if (p.val != q.val) {
             return false;
         }
-        
-        int size = list.size();
-              
-        for (int i=0; i<size; i++) {
-            if (!Objects.equals(list.get(i), list2.get(i))) {
-                return false;
-            }
-        }
-        
-        return true;
-        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
