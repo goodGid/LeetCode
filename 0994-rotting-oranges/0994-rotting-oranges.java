751ff09ef868d05076b02468f9f5db5edfc28c8e
Kiyong Shin <hello.goodgid@gmail.com>
2024-03-02 10:22:26 +0900

    Time: 2 ms (73.83%), Space: 42.2 MB (63.90%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,72 +1,84 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int[] dx = { 0, 0, 1, -1 };
-        int[] dy = { 1, -1, 0, 0 };
-
-        int row = grid.length;
-        int col = grid[0].length;
-        int[][] visit = new int[row][col];
-        int orange_cnt = 0;
-        int time = 0;
-
+    public int orangesRotting(int[][] map) {
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
+        int n = map.length;
+        int m = map[0].length;
+        
         Queue<int[]> q = new LinkedList<>();
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 1) {
-                    orange_cnt++;
-                } else if (grid[i][j] == 2) {
-                    q.add(new int[] { i, j });
+        
+        int freshCnt = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (map[i][j] == 1) {
+                    freshCnt++;
+                }
+                if (map[i][j] == 2) {
+                    q.add(new int[]{i,j});
                 }
             }
         }
-
+        
+        int cnt = -1;
         while (!q.isEmpty()) {
             int size = q.size();
-            boolean isWork = false;
-            for (int s = 0; s < size; s++) {
+            if (size > 0) {
+                cnt++;
+            }
+            
+            for (int i=0; i<size; i++) {
                 int[] node = q.poll();
                 int x = node[0];
                 int y = node[1];
-
-                for (int i = 0; i < 4; i++) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-                    if (isAccess(nx, ny, grid, visit)) {
-                        isWork = true;
-                        orange_cnt--;
-                        visit[nx][ny] = 1;
-                        q.add(new int[] { nx, ny });
+                
+                for (int k=0; k<4; k++) {
+                    int nx = x + dx[k];
+                    int ny = y + dy[k];
+                    
+                    if (!isFresh(map, nx, ny)) {
+                        continue;
                     }
+                    freshCnt--;
+                    map[nx][ny] = 2;
+                    q.add(new int[] {nx, ny});
                 }
             }
-            if (isWork) {
-                time++;
-            }
         }
-
-        if (orange_cnt == 0) {
-            return time;
-        } else {
-            return -1;
+        if (freshCnt == 0 && cnt == -1) {
+            return 0;
         }
+        if (freshCnt == 0) {
+            return cnt;
+        }
+        return -1;
     }
-
-    private boolean isAccess(int x, int y, int[][] grid, int[][] visit) {
-        int row = grid.length;
-        int col = grid[0].length;
-
-        if (x < 0 || x >= row || y < 0 || y >= col) {
+    
+    private boolean isFresh(int[][] map, int x, int y) {
+        int n = map.length;
+        int m = map[0].length;
+        
+        if (x < 0 || y < 0 || x >= n || y >= m) {
             return false;
         }
-
-        if (visit[x][y] == 1) {
+        
+        if (map[x][y] != 1) {
             return false;
         }
-
-        if (grid[x][y] != 1) {
-            return false;
-        }
+        
         return true;
+    }
+    
+    private void print(int[][] map) {
+        int n = map.length;
+        int m = map[0].length;
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
     }
 }
