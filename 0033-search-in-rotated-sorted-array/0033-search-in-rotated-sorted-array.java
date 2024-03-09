b1341e15d66cab832950a895d13553f4b76f0b89
Kiyong Shin <hello.goodgid@gmail.com>
2024-03-09 11:25:49 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (54.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,13 @@
+// 240309 : 다른 사람 코드보다 궁금해서 제출
+
 class Solution {
     public int search(int[] nums, int target) {
-        return search(nums, 0, nums.length-1, target);
-    }
-    
-    public int search(int[] nums, int l, int r, int target) {
-        int mid = 0;
-        
-        while (l <= r) {
-            mid = (l+r) / 2;
-            
-            if (nums[mid] == target) {
-                return mid;
-            }
-            
-            if (nums[l] <= nums[mid] && nums[mid] <= nums[r]) {
-                if (nums[mid] == target) {
-                    return mid;
-                } else if (nums[mid] < target) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            } else { // 정렬 안되어있는 상태
-                int leftRangeAns = search(nums, l, mid-1, target);
-                int rightRangeAns = search(nums, mid+1, r, target);
-                
-                if (leftRangeAns != -1) {
-                    return leftRangeAns;
-                }
-                if (rightRangeAns != -1) {
-                    return rightRangeAns;
-                }
-                return -1;
-            }
+        int index = -1;
+        int minValue = 100000;
+        int minIndex = -1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target) index = i;
         }
-        return -1;
+        return index;
     }
 }
