e25f347d76aa57e0e08df772d5ea9534dc6659df
Kiyong Shin <hello.goodgid@gmail.com>
2024-03-20 23:05:24 +0900

    Time: 2 ms (99.10%), Space: 45.2 MB (88.52%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,28 @@
+// ref : solution
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int len = tasks.length;
+        int[] arr = new int[26];
+
+        for (int i = 0; i < len; i++) {
+            arr[tasks[i] - 'A']++;
+        }
+
+        int max = arr[0];
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (max == arr[i]) {
+                count++;
+            }
+        }
+        return Math.max(len, (max - 1) * (n + 1) + count);
+    }
+}
