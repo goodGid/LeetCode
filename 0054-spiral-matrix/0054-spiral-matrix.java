89cb5a4a3e9fbbce2ba0d1686b72e656ea73500f
Kiyong Shin <hello.goodgid@gmail.com>
2024-05-26 19:58:33 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (20.40%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,41 +1,46 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int[] dx = { 0, 1, 0, -1 };
-        int[] dy = { 1, 0, -1, 0 };
         List<Integer> ans = new ArrayList<>();
-
-        int x = 0;
-        int y = 0;
+        List<int[]> list = new LinkedList<>();
+        int[][] visit = new int[matrix.length][matrix[0].length];
+        
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
         int dir = 0;
-        while (true) {
-            if (!isRange(matrix, x, y)) {
-                break;
-            }
-
+        
+        list.add(new int[]{0,0});
+        
+        while(!list.isEmpty()) {
+            int[] node = list.get(0);
+            int x = node[0];
+            int y = node[1];
+            list.remove(0);
+            visit[x][y] = 1;
             ans.add(matrix[x][y]);
-            matrix[x][y] = -101;
-            if (isRange(matrix, x + dx[dir], y + dy[dir])) {
-                x += dx[dir];
-                y += dy[dir];
-            } else {
-                dir = (dir + 1) % 4;
-                x += dx[dir];
-                y += dy[dir];
+            
+            for (int i=0; i<=1; i++) {
+                dir = (dir+i) % 4;
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                
+                if (isRange(matrix, nx, ny)) {
+                    if (visit[nx][ny] == 0) {
+                        list.add(new int[]{nx, ny});
+                        break;
+                    }
+                }
             }
         }
-
         return ans;
     }
-
+    
     private boolean isRange(int[][] map, int x, int y) {
-        if (x < 0 || x >= map.length || y < 0 || y >= map[0].length) {
-            return false;
+        int xLen = map.length;
+        int yLen = map[0].length;
+        
+        if (x >= 0 && x < xLen && y >= 0 && y < yLen) {
+            return true;
         }
-
-        if (map[x][y] == -101) {
-            return false;
-        }
-        return true;
+        return false;
     }
-
 }
